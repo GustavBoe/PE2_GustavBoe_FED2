ed002573a071e5cf3fccf7c2438fd9ddebd27e33
GustavBoe <gustavhugoboe@icloud.com>
2026-04-22 10:50:38 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div className='flex flex-col'>
+    <h1 className="text-3xl font-bold underline text-blue-600 self-center border"> PE2</h1>
+   </div>
+  )
+}
+
+export default App
