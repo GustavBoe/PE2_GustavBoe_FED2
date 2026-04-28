efa2de74b005320b0221650466915b609fca2609
GustavBoe <gustavhugoboe@icloud.com>
2026-04-28 13:37:30 +0200

    Added simple routes for navigation sake.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom';
 import LoginPage from '@/pages/login'
-
+import RegisterPage from '@/pages/register';
 
 function App() {
   
 
   return (
-   <div className='flex flex-col'>
-
-<LoginPage/>
+   <div>
+    <Routes>
+      <Route path="/auth/login" element={<LoginPage/>} />
+      <Route path="/auth/register" element={<RegisterPage/>} />
+    </Routes>
    </div>
   )
 }
