f064405a12c80e0c26991429c78099599eeee9aa
GustavBoe <gustavhugoboe@icloud.com>
2026-05-23 18:56:20 +0200

    Fixing width for effect.

--- src/features/HomeView.tsx
+++ src/features/HomeView.tsx
@@ -40,14 +40,14 @@ export default function HomeView(){
   }, [])
 
   return(
-    <div>
+    <div className="w-full">
     {isLoading ? 
     <div className="flex items-center justify-center min-h-screen">
     <p className="animate-bounce text-xl text-primary font-medium">
       Holidazing...
     </p>
   </div> : (
-      <section className="flex flex-col items-center text-text  mb-20">
+      <section className="flex flex-col items-center text-text mb-20">
     <div className="relative flex justify-center text-white overflow-hidden h-165 w-full mb-10">
       <img src="https://i.imghippo.com/files/ohKo6602UDk.jpg" alt="Image" className="w-full h-full object-cover object-center blur-[1px]"/>
       <div className="absolute inset-0 bg-primary/70"></div>
